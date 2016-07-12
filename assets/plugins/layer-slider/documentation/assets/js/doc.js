bebdc3e91027a9700768c3a635b162eb9e787ec0
Felipe Rodriguez <ifelipe.rodriguez@hotmail.com>
2016-07-12 16:27:33 -0500

    probando

--- assets/plugins/layer-slider/documentation/assets/js/doc.js
+++ assets/plugins/layer-slider/documentation/assets/js/doc.js
@@ -1,10 +1,9 @@
+
+
 $(document).ready(function() {
 
-	// Default hash
-
-	if( !document.location.hash ){
-		document.location.hash = 'overview';
-	}
+	// CSS transitions
+	$('body').addClass('anim');
 
 	// Init syntax highlighter
 	SyntaxHighlighter.defaults['toolbar'] = false;
@@ -12,16 +11,15 @@ $(document).ready(function() {
 
 	// jump to deeplink
 	$('a[data-goto]').click(function(){
-		$('aside li[data-deeplink="'+$(this).data('goto')+'"]').click();
+		$('#sidebar li[data-deeplink="'+$(this).data('goto')+'"]').click();
 	});
 
 	// Init sidebar
 
-	$('aside li').click(function(e){
+	$('#sidebar li').click(function(e){
 
 		var subNav = '';
 		var hash = '';
-		var curHash = document.location.hash.split('#')[1];
 
 		$(this).addClass('active').siblings().removeClass('active');
 
@@ -36,23 +34,23 @@ $(document).ready(function() {
 			if( !pli.hasClass('active') ){
 
 				pli.addClass('active').siblings().removeClass('active');
-				$('#content div > section').removeClass('active').eq( pli.index() ).addClass('active');
+				$('#content > section').removeClass('active').eq( pli.index() ).addClass('active');
 			}
 
 			// submenu
 
-			$('#content div > section.active > article').removeClass('active').eq( $(this).index() ).addClass('active');
+			$('#content > section.active > article').removeClass('active').eq( $(this).index() ).addClass('active');
 
 			// setting variables
 
 			hash = $(this).data('deeplink');
-			subNav = ' / ' + $('aside .active .active').text();
+			subNav = ' / ' + $('#sidebar .active .active').text();
 
 		}else{
 
 			// main menu
 
-			$('#content div > section').removeClass('active').eq( $(this).index() ).addClass('active');
+			$('#content > section').removeClass('active').eq( $(this).index() ).addClass('active');
 
 			var dl = $(this);
 
@@ -62,11 +60,11 @@ $(document).ready(function() {
 
 				$(this).find('li').removeClass('active');
 				dl = $(this).find('li:first').addClass('active');
-				$('#content div > section.active > article').removeClass('active').eq(0).addClass('active');
+				$('#content > section.active > article').removeClass('active').eq(0).addClass('active');
 
 				// setting variable
 
-				subNav = ' / ' + $('aside .active .active').text();
+				subNav = ' / ' + $('#sidebar .active .active').text();
 			}
 
 			// setting variable
@@ -76,36 +74,20 @@ $(document).ready(function() {
 
 		// changing curnav
 
-		$('#curnav').text( $('aside li.active > span').text() + subNav );
+		$('#curnav').text( $('#sidebar li.active > span').text() + subNav );
 
-		if( hash !== curHash ){
+		// changing hash
 
-			// changing hash
+		document.location.hash = hash;
 
-			document.location.hash = hash;
+		// fading in
 
-			// fading in
-
-			$('#content div > section.active').css('display','none').fadeIn(200);
-
-			// scrolling to top
-
-			$('#content').scrollTop(0);
-		}
-
+		$('#content > section.active').css('display','none').fadeIn(200);
 	});
-
-	// on hash change
-
-	if( "onhashchange" in window ){
-		window.onhashchange = function(){
-			$('*[data-deeplink='+(document.location.hash).split('#')[1]+']').click();
-		}
-	}
 
 	if( document.location.hash ){
 
-		$('aside li[data-deeplink="'+document.location.hash.split('#')[1]+'"]').click();
+		$('#sidebar li[data-deeplink="'+document.location.hash.split('#')[1]+'"]').click();
 	}
 
 	if( typeof layerSliderTransitions !== 'undefined' ){
@@ -113,13 +95,6 @@ $(document).ready(function() {
 		// Init transition gallery
 		transitionGallery.init();
 	}
-
-	// Search
-
-// 	$('#content section article:contains("Re-ordering")').each(function(){
-// 		console.log('talalat')
-// //		alert( $(this).closest('h4').text() );
-// 	});
 });
 
 
